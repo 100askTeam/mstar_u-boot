e16e3cfe6fe879f31630c2be1bca12ecc9c28eec
Daniel Palmer <daniel@0x0f.com>
2020-08-16 22:10:39 +0900

    clean up mstarv7 config header

--- include/configs/mstarv7.h
+++ include/configs/mstarv7.h
@@ -3,8 +3,8 @@
  * Copyright (c) 2020 Daniel Palmer<daniel@thingy.jp>
  */
 
-#ifndef __CONFIG_H
-#define __CONFIG_H
+#ifndef __MSTARV7_CONFIG_H
+#define __MSTARV7_CONFIG_H
 
 #include "../../arch/arm/mach-mstar/include/mstarv7.h"
 
@@ -27,16 +27,7 @@
 #define CONFIG_SPL_BSS_START_ADDR	MSTAR_SRAM + CONFIG_SPL_MAX_SIZE
 #define CONFIG_SPL_BSS_MAX_SIZE		0x4000
 
-#define CONFIG_SYS_HZ_CLOCK		6000000
-#define CONFIG_EXTRA_ENV_SETTINGS	"loadaddr=0x22000000\0"\
-					"bb_noroff_fit=0x80000\0"\
-					"bb_norsz_fit=0x300000\0"\
-					"bb_noroff_rescue=0xd00000\0"\
-					"bb_norsz_rescue=0x300000\0"\
-					"bootcmd=sf probe; sf read ${loadaddr} ${bb_noroff_fit} ${bb_norsz_fit}; bootm ${loadaddr}#${bb_boardtype}${bb_config}\0"\
-					"bb_boot_failsafe=sf probe; if sf read ${loadaddr} ${bb_noroff_fit} ${bb_norsz_fit}; then bootm ${loadaddr}#${bb_boardtype}; fi\0"\
-					"bb_boot_rescue=sf probe; if sf read ${loadaddr} ${bb_noroff_rescue} ${bb_norsz_rescue}; then bootm ${loadaddr}#${bb_boardtype}; fi\0"
-
+#define CONFIG_SYS_HZ_CLOCK 6000000
 #define CONFIG_ENV_SIZE			0x2000
 #define CONFIG_ENV_OFFSET		0x2000 // 8KB into the rom partition, just in case we need to put something at the start
 #define CONFIG_ENV_SECT_SIZE		4096
