ed290d1667a1764584a21ed49b33844a757df9c2
Daniel Palmer <daniel@0x0f.com>
2020-09-14 19:56:56 +0900

    ARM: mstar: Add debugging options to boot0

--- arch/arm/include/asm/arch-mstarv7/boot0.h
+++ arch/arm/include/asm/arch-mstarv7/boot0.h
@@ -7,7 +7,7 @@
  */
 
 /* The first 4 bytes should be an instruction */
-	b reset
+	b __ipl_init
 
 #ifdef CONFIG_MSTAR_IPL
 	/* this is needed for the IPL to jump into our image */
@@ -27,6 +27,33 @@
 
 	/* this is a checksum, doesn't always need to be right */
 	.long	0x0000
+
+__ipl_init :
+#ifdef CONFIG_MSTAR_BANG_BOOT0
+	// output a bang on the console so we know we're alive
+	ldr	r0, =0x1f221000
+	ldr	r1, =0x21
+	strb	r1, [r0]
+#endif
+
+#ifdef CONFIG_MSTAR_ENABLE_JTAG_BOOT0
+	// this enables JTAG on spi0 before doing anything
+	// so we can debug the SPL
+	ldr	r0, =0x1f203c3c
+	ldr	r1, =0x2
+	str	r1, [r0]
+#endif
+
+#ifdef CONFIG_MSTAR_BREAK_BOOT0
+	// this can be used to stop the cpu before uboot crashes
+	bkpt
+#endif
+
+// what does this do??
+	ldr	r0, =0x1f2078c4
+	ldr	r1, =0x10
+	str	r1, [r0]
+	b	reset
 
 _start:
 	ARM_VECTORS
