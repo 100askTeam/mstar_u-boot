fifteenhex/u-boot

arm: mstar: clk drivers

drivers/clk/mstar/clk-clkgen-mux.c
@@ -1,0 +1,158 @@
+// SPDX-License-Identifier: GPL-2.0
+
+#include <common.h>
+#include <dm.h>
+#include <clk-uclass.h>
+#include <regmap.h>
+#include <chenxingv7.h>
+#include <dt-bindings/clock/mstar.h>
+
+#define MAX_OUTPUTS 2
+
+struct mstar_clkgen_mux_priv {
+	struct regmap *regmap;
+	struct clk clk;
+	struct clk_bulk parents;
+	int num_outputs;
+	u32 gate_shifts[MAX_OUTPUTS];
+	u32 mux_shifts[MAX_OUTPUTS];
+	u32 mux_widths[MAX_OUTPUTS];
+	u32 deglitches[MAX_OUTPUTS];
+	u32 parent_offset[MAX_OUTPUTS];
+	struct clk *parent[MAX_OUTPUTS];
+};
+
+static ulong mstar_clkgen_mux_get_rate(struct clk *clk)
+{
+	struct mstar_clkgen_mux_priv *priv = dev_get_priv(clk->dev);
+
+	return clk_get_rate(priv->parent[clk->id]);
+}
+
+static int mstar_clkgen_set_parent(struct clk *clk, struct clk *parent)
+{
+	struct mstar_clkgen_mux_priv *priv = dev_get_priv(clk->dev);
+	int clkidx;
+	u32 muxshift = priv->mux_shifts[clk->id], muxwidth = priv->mux_widths[clk->id];
+	uint mask = GENMASK(muxshift + muxwidth, muxshift);
+
+	for(clkidx = 0; clkidx < priv->parents.count; clkidx++){
+		printf("checking %p %p\n", priv->parents.clks[clkidx].dev, parent->dev);
+		if(priv->parents.clks[clkidx].dev == parent->dev){
+			printk("found parent at %d\n", clkidx);
+			goto setparent;
+		}
+	}
+
+	return -EINVAL;
+
+setparent:
+	clk_enable(&priv->parents.clks[clkidx]);
+	priv->parent[clk->id] = &priv->parents.clks[clkidx];
+	regmap_update_bits(priv->regmap, 0, mask, clkidx << muxshift);
+
+	return 0;
+}
+
+static int mstar_clkgen_mux_enable(struct clk *clk)
+{
+	struct mstar_clkgen_mux_priv *priv = dev_get_priv(clk->dev);
+	uint mask = BIT(priv->gate_shifts[clk->id]);
+
+	regmap_update_bits(priv->regmap, 0, mask, 0);
+
+	return 0;
+}
+
+static int mstar_clkgen_mux_disable(struct clk *clk)
+{
+	struct mstar_clkgen_mux_priv *priv = dev_get_priv(clk->dev);
+	uint mask = BIT(priv->gate_shifts[clk->id]);
+
+	regmap_update_bits(priv->regmap, 0, mask, mask);
+
+	return 0;
+}
+
+const struct clk_ops mstar_clkgen_mux_ops = {
+	.set_parent = mstar_clkgen_set_parent,
+	.enable = mstar_clkgen_mux_enable,
+	.disable = mstar_clkgen_mux_disable,
+	.get_rate = mstar_clkgen_mux_get_rate,
+};
+
+#define CLKGEN_UART_UART0MUX_MASK	(BIT(3) | BIT(2))
+
+static int mstar_clkgen_mux_probe(struct udevice *dev)
+{
+	struct mstar_clkgen_mux_priv *priv = dev_get_priv(dev);
+	int ret, i;
+
+	ret = regmap_init_mem_index(dev_ofnode(dev), &priv->regmap, 0);
+	if(ret)
+		goto out;
+
+	ret = clk_get_by_index(dev, 0, &priv->clk);
+	if(ret)
+		goto out;
+
+	ret = clk_get_bulk(dev, &priv->parents);
+	if(ret)
+		goto out;
+
+	priv->num_outputs = ofnode_read_string_count(dev->node,
+			MSTAR_CLKGEN_MUX_OUTPUT_NAMES);
+
+	if(priv->num_outputs <= 0 || priv->num_outputs > MAX_OUTPUTS){
+		ret = -EINVAL;
+		goto out;
+	}
+
+	ret = ofnode_read_u32_array(dev->node, MSTAR_CLKGEN_MUX_GATE_SHIFTS,
+				  priv->gate_shifts, priv->num_outputs);
+	if(ret)
+		goto out;
+
+	ret = ofnode_read_u32_array(dev->node, MSTAR_CLKGEN_MUX_MUX_SHIFTS,
+				  priv->mux_shifts, priv->num_outputs);
+	if(ret)
+		goto out;
+
+	ret = ofnode_read_u32_array(dev->node, MSTAR_CLKGEN_MUX_MUX_WIDTHS,
+				  priv->mux_widths, priv->num_outputs);
+	if(ret)
+		goto out;
+
+	memset(priv->deglitches, 0xff, sizeof(priv->deglitches));
+
+	ofnode_read_u32_array(dev->node, MSTAR_CLKGEN_MUX_DEGLITCHES,
+				  priv->deglitches, priv->num_outputs);
+
+	for(i = 0; i < priv->num_outputs; i++){
+		priv->parent[i] = &priv->parents.clks[0];
+		priv->parent_offset[i] = 0;
+	}
+
+	/*uint uartclkgen;
+	regmap_read(priv->regmap, 0, &uartclkgen);
+	uartclkgen &= ~CLKGEN_UART_UART0MUX_MASK;
+	regmap_write(priv->regmap, 0, uartclkgen);*/
+
+out:
+	return ret;
+}
+
+static const struct udevice_id mstar_clkgen_mux_ids[] = {
+	{ .compatible = "mstar,clkgen_mux", },
+	{ }
+};
+
+U_BOOT_DRIVER(mstar_clkgen_mux) = {
+	.name = "mstar_clkgen_mux",
+	.id = UCLASS_CLK,
+	.of_match = mstar_clkgen_mux_ids,
+	.probe = mstar_clkgen_mux_probe,
+	.priv_auto_alloc_size = sizeof(struct mstar_clkgen_mux_priv),
+	.ops = &mstar_clkgen_mux_ops,
+	.flags = DM_FLAG_PRE_RELOC,
+};

drivers/clk/mstar/clk-cpupll.c
@@ -1,0 +1,165 @@
+// SPDX-License-Identifier: GPL-2.0
+
+#include <common.h>
+#include <dm.h>
+#include <clk-uclass.h>
+#include <asm/io.h>
+#include <regmap.h>
+#include <syscon.h>
+
+#include <chenxingv7.h>
+
+#define CPUPLL				0x1f206400
+#define CPUPLL_44			0x44
+#define CPUPLL_48			0x48
+#define CPUPLL_CURFREQ_L		0x180
+#define CPUPLL_CURFREQ_H		0x184
+#define CPUPLL_LPF_LOW_L		0x140
+#define CPUPLL_LPF_LOW_H		0x144
+#define CPUPLL_LPF_HIGH_BOTTOM		0x148
+#define CPUPLL_LPF_HIGH_TOP		0x14c
+#define CPUPLL_LPF_TOGGLE		0x150
+#define CPUPLL_LPF_MYSTERYTWO		0x154
+#define CPUPLL_LPF_UPDATE_COUNT		0x15c
+#define CPUPLL_LPF_MYSTERYONE		0x160
+#define CPUPLL_LPF_TRANSITIONCTRL	0x164
+#define CPUPLL_LPF_LOCK			0x174
+#define CPUPLL_188			0x188
+
+#define L3BRIDGE_04			0x04
+#define L3BRIDGE_04_CLK_MIU2X_SEL	BIT(7)
+
+struct mstar_cpupll_priv {
+	struct regmap *regmap;
+	struct regmap *l3bridge;
+	struct clk clk;
+};
+
+static void mstar_cpu_clk_readback(void)
+{
+	uint16_t readback;
+
+	mstar_writew(0x0001, L3BRIDGE + L3BRIDGE_2C); //test out sel?
+	mstar_writew(0x0004, 0x1f203ddc);
+	mstar_writew(0x4004, 0x1f203dd4);
+	mstar_writew(0x0001, 0x1f203dd8);
+
+	mstar_writew(0x0000, 0x1f203dc0);
+	mstar_writew(0x8000, 0x1f203dc0);
+
+	mstar_delay(100);
+
+	readback = readw(0x1f203dc4);
+
+	printf("readback: %04x\n", readback);
+
+}
+
+#define FREQ_400 0x0067AE14
+#define FREQ_800 0x0043b3d5
+#define FREQ_1000 0x002978d4
+#define BUMPFREQ FREQ_800
+
+void mstar_bump_cpufreq(void)
+{
+	uint16_t temp1, temp2;
+
+	temp1 = readw(CPUPLL + CPUPLL_CURFREQ_L);
+	temp2 = readw(CPUPLL + CPUPLL_CURFREQ_H);
+	mstar_writew(temp1, CPUPLL + CPUPLL_LPF_LOW_L);
+	mstar_writew(temp2, CPUPLL + CPUPLL_LPF_LOW_H);
+
+	mstar_writew(BUMPFREQ & 0xFFFF, CPUPLL + CPUPLL_LPF_HIGH_BOTTOM);
+	mstar_writew((BUMPFREQ >> 16) & 0xFFFF, CPUPLL + CPUPLL_LPF_HIGH_TOP);
+
+
+	mstar_writew(0x1, CPUPLL + CPUPLL_LPF_MYSTERYONE);
+	mstar_writew(0x6, CPUPLL + CPUPLL_LPF_MYSTERYTWO);
+	mstar_writew(0x8, CPUPLL + CPUPLL_LPF_UPDATE_COUNT);
+	mstar_writew(BIT(12), CPUPLL + CPUPLL_LPF_TRANSITIONCTRL);
+
+	mstar_writew(0, CPUPLL + CPUPLL_LPF_TOGGLE);
+	mstar_writew(1, CPUPLL + CPUPLL_LPF_TOGGLE);
+
+	while (!(readw(CPUPLL + CPUPLL_LPF_LOCK))) {
+		printf("waiting for cpupll lock\n");
+	}
+
+	mstar_cpu_clk_readback();
+}
+
+static int mstar_cpupll_enable(struct clk *clk)
+{
+	struct mstar_cpupll_priv *priv = dev_get_priv(clk->dev);
+	uint temp;
+
+	regmap_write(priv->regmap, CPUPLL_48, 0x0088);
+	regmap_read(priv->regmap, CPUPLL_44, &temp);
+	regmap_write(priv->regmap, CPUPLL_44, temp | 0x0100);
+
+	regmap_write(priv->regmap, CPUPLL_CURFREQ_L, 0xAE14);
+	regmap_write(priv->regmap, CPUPLL_CURFREQ_H, 0x0067);
+
+	regmap_write(priv->regmap, CPUPLL_188, 1);
+	regmap_write(priv->regmap, CPUPLL_44, temp & ~0x0100);
+
+	mdelay(1000);
+
+	mstar_writew(0x0001, 0x1f2041f0);
+
+	regmap_read(priv->l3bridge, L3BRIDGE_04, &temp);
+	regmap_write(priv->l3bridge, L3BRIDGE_04, temp | L3BRIDGE_04_CLK_MIU2X_SEL);
+	mdelay(1000);
+
+	return 0;
+}
+
+static int mstar_cpupll_disable(struct clk *clk)
+{
+	return 0;
+}
+
+const struct clk_ops mstar_cpupll_ops = {
+	.enable = mstar_cpupll_enable,
+	.disable = mstar_cpupll_disable,
+};
+
+static int mstar_cpupll_probe(struct udevice *dev)
+{
+	struct mstar_cpupll_priv *priv = dev_get_priv(dev);
+	int ret;
+
+	ret = regmap_init_mem_index(dev_ofnode(dev), &priv->regmap, 0);
+	if(ret)
+		goto out;
+
+	priv->l3bridge = syscon_regmap_lookup_by_phandle(dev, "mstar,l3bridge");
+	if(!priv->l3bridge){
+		ret = -ENODEV;
+		goto out;
+	}
+
+	ret = clk_get_by_index(dev, 0, &priv->clk);
+	if(ret)
+		goto out;
+
+	clk_enable(&priv->clk);
+
+out:
+	return ret;
+}
+
+static const struct udevice_id mstar_cpupll_ids[] = {
+	{ .compatible = "mstar,cpupll", },
+	{ }
+};
+
+U_BOOT_DRIVER(mstar_cpupll) = {
+	.name = "mstar_cpupll",
+	.id = UCLASS_CLK,
+	.of_match = mstar_cpupll_ids,
+	.probe = mstar_cpupll_probe,
+	.priv_auto_alloc_size = sizeof(struct mstar_cpupll_priv),
+	.ops = &mstar_cpupll_ops,
+	.flags = DM_FLAG_PRE_RELOC,
+};

drivers/clk/mstar/clk-miupll.c
@@ -1,0 +1,101 @@
+// SPDX-License-Identifier: GPL-2.0
+
+#include <common.h>
+#include <dm.h>
+#include <clk.h>
+#include <clk-uclass.h>
+#include <regmap.h>
+#include <chenxingv7.h>
+
+#define MAYBEPLL1_04			0x4
+#define MAYBEPLL1_08			0x8
+#define MAYBEPLL1_0C			0xc
+#define MAYBEPLL1_10			0x10
+
+
+struct mstar_miupll_magicnumbers {
+	u16 pll_magic_08, pll_magic_0c, pll_magic_10;
+};
+
+struct mstar_miupll_priv {
+	struct regmap *regmap;
+	struct mstar_miupll_magicnumbers magicnumbers;
+};
+
+static int mstar_miupll_enable(struct clk *clk)
+{
+	struct mstar_miupll_priv *priv = dev_get_priv(clk->dev);
+
+	/* seems to be power on, i3 ipl has this after setting the registers
+	 * the i2 and m5 ipls has this before setting the registers.
+	 */
+	regmap_write(priv->regmap, MAYBEPLL1_04, 0);
+	// vendor code has a delay
+	mstar_delay(1000);
+
+	if(priv->magicnumbers.pll_magic_08 > 0)
+		regmap_write(priv->regmap, MAYBEPLL1_08, priv->magicnumbers.pll_magic_08);
+	if(priv->magicnumbers.pll_magic_0c > 0)
+		regmap_write(priv->regmap, MAYBEPLL1_0C, priv->magicnumbers.pll_magic_0c);
+	if(priv->magicnumbers.pll_magic_10 > 0)
+		regmap_write(priv->regmap, MAYBEPLL1_10, priv->magicnumbers.pll_magic_10);
+
+	return 0;
+}
+
+static int mstar_miupll_disable(struct clk *clk)
+{
+	return 0;
+}
+
+const struct clk_ops mstar_miupll_ops = {
+	.enable = mstar_miupll_enable,
+	.disable = mstar_miupll_disable,
+};
+
+static int mstar_miupll_probe(struct udevice *dev)
+{
+	struct mstar_miupll_priv *priv = dev_get_priv(dev);
+	int ret;
+
+	ret = regmap_init_mem_index(dev_ofnode(dev), &priv->regmap, 0);
+	if(ret)
+		goto out;
+
+	priv->magicnumbers.pll_magic_08 = -1;
+	priv->magicnumbers.pll_magic_0c = -1;
+	priv->magicnumbers.pll_magic_10 = -1;
+
+	switch(mstar_chiptype()){
+		case CHIPTYPE_MSC313E:
+			priv->magicnumbers.pll_magic_0c = 0x22c;
+		break;
+		case CHIPTYPE_SSC8336:
+		case CHIPTYPE_SSC8336N:
+			priv->magicnumbers.pll_magic_08 = 0x0100;
+			priv->magicnumbers.pll_magic_0c = 0x0216;
+			priv->magicnumbers.pll_magic_10 = 0x0010;
+		break;
+		default:
+			dev_err(dev, "Don't know miu pll config\n");
+			ret = -EINVAL;
+			goto out;
+	}
+out:
+	return ret;
+}
+
+static const struct udevice_id mstar_miupll_ids[] = {
+	{ .compatible = "mstar,miupll", },
+	{ }
+};
+
+U_BOOT_DRIVER(mstar_miupll) = {
+	.name = "mstar_miupll",
+	.id = UCLASS_CLK,
+	.of_match = mstar_miupll_ids,
+	.probe = mstar_miupll_probe,
+	.priv_auto_alloc_size = sizeof(struct mstar_miupll_priv),
+	.ops = &mstar_miupll_ops,
+	.flags = DM_FLAG_PRE_RELOC,
+};

100askTeam/mstar_u-boot

drivers/clk/mstar/clk-clkgen-mux.c
@@ -1,0 +1,158 @@
+// SPDX-License-Identifier: GPL-2.0
+
+#include <common.h>
+#include <dm.h>
+#include <clk-uclass.h>
+#include <regmap.h>
+#include <chenxingv7.h>
+#include <dt-bindings/clock/mstar.h>
+
+#define MAX_OUTPUTS 2
+
+struct mstar_clkgen_mux_priv {
+	struct regmap *regmap;
+	struct clk clk;
+	struct clk_bulk parents;
+	int num_outputs;
+	u32 gate_shifts[MAX_OUTPUTS];
+	u32 mux_shifts[MAX_OUTPUTS];
+	u32 mux_widths[MAX_OUTPUTS];
+	u32 deglitches[MAX_OUTPUTS];
+	u32 parent_offset[MAX_OUTPUTS];
+	struct clk *parent[MAX_OUTPUTS];
+};
+
+static ulong mstar_clkgen_mux_get_rate(struct clk *clk)
+{
+	struct mstar_clkgen_mux_priv *priv = dev_get_priv(clk->dev);
+
+	return clk_get_rate(priv->parent[clk->id]);
+}
+
+static int mstar_clkgen_set_parent(struct clk *clk, struct clk *parent)
+{
+	struct mstar_clkgen_mux_priv *priv = dev_get_priv(clk->dev);
+	int clkidx;
+	u32 muxshift = priv->mux_shifts[clk->id], muxwidth = priv->mux_widths[clk->id];
+	uint mask = GENMASK(muxshift + muxwidth, muxshift);
+
+	for(clkidx = 0; clkidx < priv->parents.count; clkidx++){
+		printf("checking %p %p\n", priv->parents.clks[clkidx].dev, parent->dev);
+		if(priv->parents.clks[clkidx].dev == parent->dev){
+			printk("found parent at %d\n", clkidx);
+			goto setparent;
+		}
+	}
+
+	return -EINVAL;
+
+setparent:
+	clk_enable(&priv->parents.clks[clkidx]);
+	priv->parent[clk->id] = &priv->parents.clks[clkidx];
+	regmap_update_bits(priv->regmap, 0, mask, clkidx << muxshift);
+
+	return 0;
+}
+
+static int mstar_clkgen_mux_enable(struct clk *clk)
+{
+	struct mstar_clkgen_mux_priv *priv = dev_get_priv(clk->dev);
+	uint mask = BIT(priv->gate_shifts[clk->id]);
+
+	regmap_update_bits(priv->regmap, 0, mask, 0);
+
+	return 0;
+}
+
+static int mstar_clkgen_mux_disable(struct clk *clk)
+{
+	struct mstar_clkgen_mux_priv *priv = dev_get_priv(clk->dev);
+	uint mask = BIT(priv->gate_shifts[clk->id]);
+
+	regmap_update_bits(priv->regmap, 0, mask, mask);
+
+	return 0;
+}
+
+const struct clk_ops mstar_clkgen_mux_ops = {
+	.set_parent = mstar_clkgen_set_parent,
+	.enable = mstar_clkgen_mux_enable,
+	.disable = mstar_clkgen_mux_disable,
+	.get_rate = mstar_clkgen_mux_get_rate,
+};
+
+#define CLKGEN_UART_UART0MUX_MASK	(BIT(3) | BIT(2))
+
+static int mstar_clkgen_mux_probe(struct udevice *dev)
+{
+	struct mstar_clkgen_mux_priv *priv = dev_get_priv(dev);
+	int ret, i;
+
+	ret = regmap_init_mem_index(dev_ofnode(dev), &priv->regmap, 0);
+	if(ret)
+		goto out;
+
+	ret = clk_get_by_index(dev, 0, &priv->clk);
+	if(ret)
+		goto out;
+
+	ret = clk_get_bulk(dev, &priv->parents);
+	if(ret)
+		goto out;
+
+	priv->num_outputs = ofnode_read_string_count(dev->node_,
+			MSTAR_CLKGEN_MUX_OUTPUT_NAMES);
+
+	if(priv->num_outputs <= 0 || priv->num_outputs > MAX_OUTPUTS){
+		ret = -EINVAL;
+		goto out;
+	}
+
+	ret = ofnode_read_u32_array(dev->node_, MSTAR_CLKGEN_MUX_GATE_SHIFTS,
+				  priv->gate_shifts, priv->num_outputs);
+	if(ret)
+		goto out;
+
+	ret = ofnode_read_u32_array(dev->node_, MSTAR_CLKGEN_MUX_MUX_SHIFTS,
+				  priv->mux_shifts, priv->num_outputs);
+	if(ret)
+		goto out;
+
+	ret = ofnode_read_u32_array(dev->node_, MSTAR_CLKGEN_MUX_MUX_WIDTHS,
+				  priv->mux_widths, priv->num_outputs);
+	if(ret)
+		goto out;
+
+	memset(priv->deglitches, 0xff, sizeof(priv->deglitches));
+
+	ofnode_read_u32_array(dev->node_, MSTAR_CLKGEN_MUX_DEGLITCHES,
+				  priv->deglitches, priv->num_outputs);
+
+	for(i = 0; i < priv->num_outputs; i++){
+		priv->parent[i] = &priv->parents.clks[0];
+		priv->parent_offset[i] = 0;
+	}
+
+	/*uint uartclkgen;
+	regmap_read(priv->regmap, 0, &uartclkgen);
+	uartclkgen &= ~CLKGEN_UART_UART0MUX_MASK;
+	regmap_write(priv->regmap, 0, uartclkgen);*/
+
+out:
+	return ret;
+}
+
+static const struct udevice_id mstar_clkgen_mux_ids[] = {
+	{ .compatible = "mstar,clkgen_mux", },
+	{ }
+};
+
+U_BOOT_DRIVER(mstar_clkgen_mux) = {
+	.name = "mstar_clkgen_mux",
+	.id = UCLASS_CLK,
+	.of_match = mstar_clkgen_mux_ids,
+	.probe = mstar_clkgen_mux_probe,
+	.priv_auto = sizeof(struct mstar_clkgen_mux_priv),
+	.ops = &mstar_clkgen_mux_ops,
+	.flags = DM_FLAG_PRE_RELOC,
+};

drivers/clk/mstar/clk-cpupll.c
@@ -1,0 +1,165 @@
+// SPDX-License-Identifier: GPL-2.0
+
+#include <common.h>
+#include <dm.h>
+#include <clk-uclass.h>
+#include <asm/io.h>
+#include <regmap.h>
+#include <syscon.h>
+
+#include <chenxingv7.h>
+
+#define CPUPLL				0x1f206400
+#define CPUPLL_44			0x44
+#define CPUPLL_48			0x48
+#define CPUPLL_CURFREQ_L		0x180
+#define CPUPLL_CURFREQ_H		0x184
+#define CPUPLL_LPF_LOW_L		0x140
+#define CPUPLL_LPF_LOW_H		0x144
+#define CPUPLL_LPF_HIGH_BOTTOM		0x148
+#define CPUPLL_LPF_HIGH_TOP		0x14c
+#define CPUPLL_LPF_TOGGLE		0x150
+#define CPUPLL_LPF_MYSTERYTWO		0x154
+#define CPUPLL_LPF_UPDATE_COUNT		0x15c
+#define CPUPLL_LPF_MYSTERYONE		0x160
+#define CPUPLL_LPF_TRANSITIONCTRL	0x164
+#define CPUPLL_LPF_LOCK			0x174
+#define CPUPLL_188			0x188
+
+#define L3BRIDGE_04			0x04
+#define L3BRIDGE_04_CLK_MIU2X_SEL	BIT(7)
+
+struct mstar_cpupll_priv {
+	struct regmap *regmap;
+	struct regmap *l3bridge;
+	struct clk clk;
+};
+
+static void mstar_cpu_clk_readback(void)
+{
+	uint16_t readback;
+
+	mstar_writew(0x0001, L3BRIDGE + L3BRIDGE_2C); //test out sel?
+	mstar_writew(0x0004, 0x1f203ddc);
+	mstar_writew(0x4004, 0x1f203dd4);
+	mstar_writew(0x0001, 0x1f203dd8);
+
+	mstar_writew(0x0000, 0x1f203dc0);
+	mstar_writew(0x8000, 0x1f203dc0);
+
+	mstar_delay(100);
+
+	readback = readw(0x1f203dc4);
+
+	printf("readback: %04x\n", readback);
+
+}
+
+#define FREQ_400 0x0067AE14
+#define FREQ_800 0x0043b3d5
+#define FREQ_1000 0x002978d4
+#define BUMPFREQ FREQ_800
+
+void mstar_bump_cpufreq(void)
+{
+	uint16_t temp1, temp2;
+
+	temp1 = readw(CPUPLL + CPUPLL_CURFREQ_L);
+	temp2 = readw(CPUPLL + CPUPLL_CURFREQ_H);
+	mstar_writew(temp1, CPUPLL + CPUPLL_LPF_LOW_L);
+	mstar_writew(temp2, CPUPLL + CPUPLL_LPF_LOW_H);
+
+	mstar_writew(BUMPFREQ & 0xFFFF, CPUPLL + CPUPLL_LPF_HIGH_BOTTOM);
+	mstar_writew((BUMPFREQ >> 16) & 0xFFFF, CPUPLL + CPUPLL_LPF_HIGH_TOP);
+
+
+	mstar_writew(0x1, CPUPLL + CPUPLL_LPF_MYSTERYONE);
+	mstar_writew(0x6, CPUPLL + CPUPLL_LPF_MYSTERYTWO);
+	mstar_writew(0x8, CPUPLL + CPUPLL_LPF_UPDATE_COUNT);
+	mstar_writew(BIT(12), CPUPLL + CPUPLL_LPF_TRANSITIONCTRL);
+
+	mstar_writew(0, CPUPLL + CPUPLL_LPF_TOGGLE);
+	mstar_writew(1, CPUPLL + CPUPLL_LPF_TOGGLE);
+
+	while (!(readw(CPUPLL + CPUPLL_LPF_LOCK))) {
+		printf("waiting for cpupll lock\n");
+	}
+
+	mstar_cpu_clk_readback();
+}
+
+static int mstar_cpupll_enable(struct clk *clk)
+{
+	struct mstar_cpupll_priv *priv = dev_get_priv(clk->dev);
+	uint temp;
+
+	regmap_write(priv->regmap, CPUPLL_48, 0x0088);
+	regmap_read(priv->regmap, CPUPLL_44, &temp);
+	regmap_write(priv->regmap, CPUPLL_44, temp | 0x0100);
+
+	regmap_write(priv->regmap, CPUPLL_CURFREQ_L, 0xAE14);
+	regmap_write(priv->regmap, CPUPLL_CURFREQ_H, 0x0067);
+
+	regmap_write(priv->regmap, CPUPLL_188, 1);
+	regmap_write(priv->regmap, CPUPLL_44, temp & ~0x0100);
+
+	mdelay(1000);
+
+	mstar_writew(0x0001, 0x1f2041f0);
+
+	regmap_read(priv->l3bridge, L3BRIDGE_04, &temp);
+	regmap_write(priv->l3bridge, L3BRIDGE_04, temp | L3BRIDGE_04_CLK_MIU2X_SEL);
+	mdelay(1000);
+
+	return 0;
+}
+
+static int mstar_cpupll_disable(struct clk *clk)
+{
+	return 0;
+}
+
+const struct clk_ops mstar_cpupll_ops = {
+	.enable = mstar_cpupll_enable,
+	.disable = mstar_cpupll_disable,
+};
+
+static int mstar_cpupll_probe(struct udevice *dev)
+{
+	struct mstar_cpupll_priv *priv = dev_get_priv(dev);
+	int ret;
+
+	ret = regmap_init_mem_index(dev_ofnode(dev), &priv->regmap, 0);
+	if(ret)
+		goto out;
+
+	priv->l3bridge = syscon_regmap_lookup_by_phandle(dev, "mstar,l3bridge");
+	if(!priv->l3bridge){
+		ret = -ENODEV;
+		goto out;
+	}
+
+	ret = clk_get_by_index(dev, 0, &priv->clk);
+	if(ret)
+		goto out;
+
+	clk_enable(&priv->clk);
+
+out:
+	return ret;
+}
+
+static const struct udevice_id mstar_cpupll_ids[] = {
+	{ .compatible = "mstar,cpupll", },
+	{ }
+};
+
+U_BOOT_DRIVER(mstar_cpupll) = {
+	.name = "mstar_cpupll",
+	.id = UCLASS_CLK,
+	.of_match = mstar_cpupll_ids,
+	.probe = mstar_cpupll_probe,
+	.priv_auto = sizeof(struct mstar_cpupll_priv),
+	.ops = &mstar_cpupll_ops,
+	.flags = DM_FLAG_PRE_RELOC,
+};

drivers/clk/mstar/clk-miupll.c
@@ -1,0 +1,101 @@
+// SPDX-License-Identifier: GPL-2.0
+
+#include <common.h>
+#include <dm.h>
+#include <clk.h>
+#include <clk-uclass.h>
+#include <regmap.h>
+#include <chenxingv7.h>
+
+#define MAYBEPLL1_04			0x4
+#define MAYBEPLL1_08			0x8
+#define MAYBEPLL1_0C			0xc
+#define MAYBEPLL1_10			0x10
+
+
+struct mstar_miupll_magicnumbers {
+	u16 pll_magic_08, pll_magic_0c, pll_magic_10;
+};
+
+struct mstar_miupll_priv {
+	struct regmap *regmap;
+	struct mstar_miupll_magicnumbers magicnumbers;
+};
+
+static int mstar_miupll_enable(struct clk *clk)
+{
+	struct mstar_miupll_priv *priv = dev_get_priv(clk->dev);
+
+	/* seems to be power on, i3 ipl has this after setting the registers
+	 * the i2 and m5 ipls has this before setting the registers.
+	 */
+	regmap_write(priv->regmap, MAYBEPLL1_04, 0);
+	// vendor code has a delay
+	mstar_delay(1000);
+
+	if(priv->magicnumbers.pll_magic_08 > 0)
+		regmap_write(priv->regmap, MAYBEPLL1_08, priv->magicnumbers.pll_magic_08);
+	if(priv->magicnumbers.pll_magic_0c > 0)
+		regmap_write(priv->regmap, MAYBEPLL1_0C, priv->magicnumbers.pll_magic_0c);
+	if(priv->magicnumbers.pll_magic_10 > 0)
+		regmap_write(priv->regmap, MAYBEPLL1_10, priv->magicnumbers.pll_magic_10);
+
+	return 0;
+}
+
+static int mstar_miupll_disable(struct clk *clk)
+{
+	return 0;
+}
+
+const struct clk_ops mstar_miupll_ops = {
+	.enable = mstar_miupll_enable,
+	.disable = mstar_miupll_disable,
+};
+
+static int mstar_miupll_probe(struct udevice *dev)
+{
+	struct mstar_miupll_priv *priv = dev_get_priv(dev);
+	int ret;
+
+	ret = regmap_init_mem_index(dev_ofnode(dev), &priv->regmap, 0);
+	if(ret)
+		goto out;
+
+	priv->magicnumbers.pll_magic_08 = -1;
+	priv->magicnumbers.pll_magic_0c = -1;
+	priv->magicnumbers.pll_magic_10 = -1;
+
+	switch(mstar_chiptype()){
+		case CHIPTYPE_MSC313E:
+			priv->magicnumbers.pll_magic_0c = 0x22c;
+		break;
+		case CHIPTYPE_SSC8336:
+		case CHIPTYPE_SSC8336N:
+			priv->magicnumbers.pll_magic_08 = 0x0100;
+			priv->magicnumbers.pll_magic_0c = 0x0216;
+			priv->magicnumbers.pll_magic_10 = 0x0010;
+		break;
+		default:
+			//dev_err(dev, "Don't know miu pll config\n");
+			ret = -EINVAL;
+			goto out;
+	}
+out:
+	return ret;
+}
+
+static const struct udevice_id mstar_miupll_ids[] = {
+	{ .compatible = "mstar,miupll", },
+	{ }
+};
+
+U_BOOT_DRIVER(mstar_miupll) = {
+	.name = "mstar_miupll",
+	.id = UCLASS_CLK,
+	.of_match = mstar_miupll_ids,
+	.probe = mstar_miupll_probe,
+	.priv_auto = sizeof(struct mstar_miupll_priv),
+	.ops = &mstar_miupll_ops,
+	.flags = DM_FLAG_PRE_RELOC,
+};